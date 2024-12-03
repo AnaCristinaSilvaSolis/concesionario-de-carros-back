efcaf778011572587548c2dc255c91bbcb3e9545
Ana Cristina Silva Solis <cristinasilvasolis09@gmail.com>
2024-12-03 14:30:22 -0500

    subiendo

---
+++ src/main/java/com/project/coches/security/Roles.java
@@ -0,0 +1,11 @@
+package com.project.coches.security;
+
+public class Roles {
+
+    public static final String CUSTOMER = "Customer";
+
+    public static final String ADMIN = "Administrator";
+
+
+
+}
